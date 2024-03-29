2f5a139d71a203c2e0112c597ae90ce935732299
vital987321 <vitaliyvelichko@gmail.com>
2024-03-29 23:34:08 +0100

    no permission classes

--- job_agency_backend/settings.py
+++ job_agency_backend/settings.py
@@ -166,7 +166,7 @@ REST_FRAMEWORK = {
         'agencyapp.api.authentication.TokenExpiredAuthantication',
     ],
     'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.IsAuthenticated',
+        # 'rest_framework.permissions.IsAuthenticated',
     ]
 }
 
