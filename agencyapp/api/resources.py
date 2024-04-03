f0a11bccff1cd829bea78fff86d8b73067af1d75
vital987321 <vitaliyvelichko@gmail.com>
2024-04-04 00:32:35 +0200

    working on django filter

--- agencyapp/api/resources.py
+++ agencyapp/api/resources.py
@@ -1,10 +1,17 @@
 from rest_framework import viewsets
 from agencyapp.api.serializers import UserSerializer, VacancySerializer, SectorSerializer, ApplicationSerializer
 from agencyapp.models import Vacancy, Application, User
+from rest_framework.filters import SearchFilter
+from django_filters.rest_framework import DjangoFilterBackend
+
 
 class VacancyViewSet(viewsets.ModelViewSet):
     queryset=Vacancy.objects.all()
     serializer_class=VacancySerializer
+    filter_backends=[DjangoFilterBackend]
+    filter_fields=('name')
+    
+
 
 class ApplicationViewSet(viewsets.ModelViewSet):
     queryset=Application.objects.all()
--- job_agency_backend/settings.py
+++ job_agency_backend/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework.authtoken',
     'corsheaders',
+    'django_filters',
 
 ]
 
@@ -167,7 +168,8 @@ REST_FRAMEWORK = {
     ],
     'DEFAULT_PERMISSION_CLASSES': [
         # 'rest_framework.permissions.IsAuthenticated',
-    ]
+    ],
+    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend',]
 }
 
 CORS_ALLOW_ALL_ORIGINS = True
