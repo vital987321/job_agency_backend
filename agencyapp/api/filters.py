18bb1121fd955952680004d8ec842abca2c90071
vital987321 <vitaliyvelichko@gmail.com>
2024-04-04 22:07:04 +0200

    aded key_search to VacancyFilter

--- agencyapp/api/filters.py
+++ agencyapp/api/filters.py
@@ -1,15 +1,20 @@
 from django_filters import rest_framework as filters
 from agencyapp.models import Vacancy
+from django.db.models import Q
 
 class VacancyFilter(filters.FilterSet):
-    name=filters.CharFilter(field_name='name', lookup_expr='icontains')
+    key_search=filters.CharFilter(method='name_or_description')
     location=filters.CharFilter(field_name='location', lookup_expr='icontains')
     salary_gte=filters.NumberFilter(field_name='salary', lookup_expr='gte')
     salary_lte=filters.NumberFilter(field_name='salary', lookup_expr='lte')
     residence_type=filters.NumberFilter(field_name='residence_type', lookup_expr='gte')
+    
     class Meta:
         model=Vacancy
-        fields=['name', 'location', 'salary_gte', 'salary_lte','residence_type']
-
+        fields=[]
+ 
+    def name_or_description(self, queryset, query_name, value):
+        return queryset.filter(Q(name__icontains=value) | Q(description__icontains=value))
+        
 
 
