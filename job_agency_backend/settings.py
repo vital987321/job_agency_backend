67a6dd7fe45639d732b58ef40294bb4e6d40485e
vital987321 <vitaliyvelichko@gmail.com>
2024-04-07 22:36:34 +0200

    pagination added

--- job_agency_backend/settings.py
+++ job_agency_backend/settings.py
@@ -179,7 +179,9 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         # 'rest_framework.permissions.IsAuthenticated',
     ],
-    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend']
+    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    'PAGE_SIZE': 10
 }
 
 CORS_ALLOW_ALL_ORIGINS = True
