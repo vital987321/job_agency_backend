a660cee9d19ec0ca03f50028d04469c02d87f749
vital987321 <vitaliyvelichko@gmail.com>
2024-09-10 14:47:25 +0200

    Corrected UserPermission

--- agencyapp/api/permissions.py
+++ agencyapp/api/permissions.py
@@ -4,29 +4,26 @@ from rest_framework import permissions
 class UserPermission(permissions.BasePermission):
 
     def has_permission(self, request, view):
-        if view.action == 'list':
-            # return request.user.is_authenticated and request.user.is_admin
+        if request.user.is_staff:
             return True
-        elif view.action == 'create':
-            return True
-        elif view.action in ['retrieve', 'update', 'partial_update', 'destroy']:
-            return True
-        else:
-            return False
-
-    def has_object_permission(self, request, view, obj):
-        # Deny actions on objects if the user is not authenticated
         if not request.user.is_authenticated:
             return False
-
-        if view.action == 'retrieve':
-            return obj == request.user or request.user.is_admin
-        elif view.action in ['update', 'partial_update']:
-            return obj == request.user or request.user.is_admin
-        elif view.action == 'destroy':
-            return obj == request.user or request.user.is_admin
-        else:
+        if request.user.is_authenticated:
+            if view.action == 'list':
+                return False
+            if view.action in ['retrieve', 'update', 'partial_update', 'destroy']:
+                return True
+        return False
+        
+    def has_object_permission(self, request, view, obj):
+        if request.user.is_staff:
+            return True
+        if not request.user.is_authenticated:
             return False
+        if request.user.is_authenticated:
+            return obj == request.user
+        return False
+
 
 
 class ApplicationPermission(permissions.BasePermission):
